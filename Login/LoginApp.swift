c9bf3ad5080319ca9d1da43aea1a9975ef2d5c9c
Alice Martins <alicemartins@outlook.com>
2025-01-28 22:56:51 -0300

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Alice Martins on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
